74ec8b7101ad2e54107e8275b2127b00b71c4889
jaimelombanag <alombana24@hotmail.com>
2018-06-14 19:56:43 -0500

    Creacion de ViewControllers y clases

---
+++ Modulo2NextU/Modulo2NextU/KybosCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  KybosCollectionViewCell.swift
+//  Modulo2NextU
+//
+//  Created by Jaime Lombana on 14/06/18.
+//  Copyright © 2018 Jaime Lombana. All rights reserved.
+//
+
+import UIKit
+
+class KybosCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Modulo2NextU/Modulo2NextU/KybosViewController.swift
@@ -0,0 +1,35 @@
+//
+//  KybosViewController.swift
+//  Modulo2NextU
+//
+//  Created by Jaime Lombana on 14/06/18.
+//  Copyright © 2018 Jaime Lombana. All rights reserved.
+//
+
+import UIKit
+
+class KybosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

1409639c5868e5a6bf98cc6c47ce3ee9a4932e8b
jaimelombanag <alombana24@hotmail.com>
2018-06-20 21:13:15 -0500

    posible scroll

--- Modulo2NextU/Modulo2NextU/FastoViewController.swift
+++ Modulo2NextU/Modulo2NextU/FastoViewController.swift
@@ -15,6 +15,7 @@ class FastoViewController: UIViewController,  UICollectionViewDelegate, UICollec
     @IBOutlet weak var titleFasto: UILabel!
     var imageArray = [UIImage(named: "fasto1"), UIImage(named: "fasto2"), UIImage(named: "fasto3"), UIImage(named: "fasto4")]
     @IBOutlet weak var descriptionFasto: UITextView!
+    @IBOutlet weak var imagesFasto: UIScrollView!
     
     
 
@@ -23,6 +24,45 @@ class FastoViewController: UIViewController,  UICollectionViewDelegate, UICollec
 
         
         titleFasto.text = "Fasto"
+        
+        
+        
+        let imagenesFasto = ["fasto1", "fasto2", "fasto3", "fasto4"]
+        
+        let imageWidth:CGFloat = 150
+        let imageHeight:CGFloat = 150
+        var yPosition:CGFloat = 0
+        var ScrollViewContentSize:CGFloat = 150
+        
+        
+        
+        
+        
+        
+        for index in 0 ..< imagenesFasto.count {
+            
+            let imageFasto:UIImage = UIImage(named: imagenesFasto[index])!
+            let imageFastiView:UIImageView = UIImageView()
+            imageFastiView.image = imageFasto
+            
+            imageFastiView.frame.size.width = imageWidth
+            imageFastiView.frame.size.height = imageHeight
+            imageFastiView.frame.origin.x = yPosition
+            imageFastiView.frame.origin.y = 10
+            
+            imagesFasto.addSubview(imageFastiView)
+            
+            yPosition += imageHeight
+            ScrollViewContentSize += imageHeight
+
+            imagesFasto.contentSize = CGSize(width: imageWidth, height: ScrollViewContentSize)
+            
+        }
+        
+        
+        
+        
+        
         // Do any additional setup after loading the view.
     }
 
